6dcf048f0e129370164ce3454acc8b51586244a3
Utsav <utsavns96@gmail.com>
2025-06-25 00:15:31 -0500

    converting raw json into .csv file

--- dataParsing.py
+++ dataParsing.py
@@ -1,1 +1,9 @@
-print("hello world")
+import json
+import pandas as pd
+
+with open('data/playlist.json') as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data)
+# print(df.head(10))
+df.to_csv('data/playlist.csv', index=False)
